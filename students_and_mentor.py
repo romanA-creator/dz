9a80187529ee386c705442b13edf4ee1305b33b6
netology <arhipenkovr@mail.ru>
2024-12-09 21:29:09 +0300

    dz1

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -6,30 +6,27 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
- 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+
+    def __str__(self):
+        return f"Mentor: {self.name} {self.surname}"
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def __str__(self):
+        avg_grade = self.calculate_average_grade()
+        return f"Lecturer: {self.name} {self.surname}, Average Grade: {avg_grade:.2f}"
+
+class Reviewer(Mentor):
+    def __str__(self):
+        return f"Reviewer: {self.name} {self.surname}"
+    
